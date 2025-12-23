db287487678393e3602d96ee0aff183df137f375
Ali Alibaba <alialbaba114@gmail.com>
2025-12-23 08:43:36 +0330

    Add geocoding search using Fetch API

--- script.js
+++ script.js
@@ -15,5 +15,36 @@ const map = new ol.Map({
     zoom: 2
   })
 });
+const searchBtn = document.getElementById("searchBtn");
+const addressInput = document.getElementById("address");
+
+// TODO: replace with your real API key
+const GEOCODING_API_KEY = "YOUR_API_KEY_HERE";
+
+searchBtn.addEventListener("click", async () => {
+  const address = addressInput.value;
+  if (!address) return alert("Enter an address");
+
+  const url = `https://open.mapquestapi.com/geocoding/v1/address?key=${GEOCODING_API_KEY}&location=${address}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const location = data.results[0].locations[0].latLng;
+    const lon = location.lng;
+    const lat = location.lat;
+
+    map.getView().animate({
+      center: ol.proj.fromLonLat([lon, lat]),
+      zoom: 12,
+      duration: 1000
+    });
+
+  } catch (err) {
+    alert("Geocoding failed");
+    console.error(err);
+  }
+});
 
   
